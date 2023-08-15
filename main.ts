ccf6d6831516fdeb3ff8bf57ee0fecf124b05168
larryfast <larry@legible.com>
2023-08-15 16:26:25 -0700

    working!

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ input.onButtonPressed(Button.A, function () {
     datalogger.deleteLog()
 })
 radio.onReceivedValue(function (name, value) {
+    led.toggle(0, 0)
+    datalogger.mirrorToSerial(true)
     datalogger.log(datalogger.createCV(name, value))
 })
 radio.setGroup(12)
-basic.showIcon(IconNames.House)
+basic.showIcon(IconNames.Yes)
+datalogger.mirrorToSerial(true)
 datalogger.setColumnTitles(
 "tovut",
 "dummy"
 )
-datalogger.mirrorToSerial(true)
