576e41712f280b40932b3ea5ddc780875639cb3a
larryfast <larry@legible.com>
2023-10-16 16:17:52 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,9 @@ input.onButtonPressed(Button.A, function () {
 })
 radio.onReceivedValue(function (name, value) {
     led.toggle(0, 0)
-    datalogger.mirrorToSerial(true)
+    serial.writeValue(name, value)
     datalogger.log(datalogger.createCV(name, value))
 })
 radio.setGroup(12)
+basic.showString(control.deviceName())
 basic.showIcon(IconNames.Yes)
-datalogger.mirrorToSerial(true)
-datalogger.setColumnTitles(
-"tovut",
-"dummy"
-)
